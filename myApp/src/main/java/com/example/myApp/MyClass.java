6f105a5f8de66a985b315b5ae861f651b414a11c
Ashwini A <ashwinishaiva385@gmail.com>
2026-08-22 19:38:50 +0530

    Added spring  boot project using @Primary and @Qualifier annotation

--- myApp/src/main/java/com/example/myApp/MyClass.java
+++ myApp/src/main/java/com/example/myApp/MyClass.java
@@ -1,15 +1,17 @@
 package com.example.myApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 @Component
 public class MyClass
 { @Autowired 
-  private Laptop lap;
+  @Qualifier("laptop")
+  private Computer com;
   
   public void bulid()
   {
-    lap.compile();
+    com.compile();
     System.out.println("I am building project");
   }
 }
