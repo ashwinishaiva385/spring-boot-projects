bb93ebdd9c212080aee906d205cc9a6fca6cf0ff
Ashwini A <ashwinishaiva385@gmail.com>
2026-08-22 19:07:27 +0530

    Added Spring boot object using autowired annotation

---
+++ myApp/src/main/java/com/example/myApp/Laptop.java
@@ -0,0 +1,11 @@
+package com.example.myApp;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop 
+{
+    public void compile()
+    {
+        System.out.println(".....Compiling.....");
+    }
+}
--- myApp/src/main/java/com/example/myApp/MyClass.java
+++ myApp/src/main/java/com/example/myApp/MyClass.java
@@ -1,11 +1,15 @@
 package com.example.myApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 @Component
 public class MyClass
-{ 
+{ @Autowired 
+  private Laptop lap;
+  
   public void bulid()
   {
+    lap.compile();
     System.out.println("I am building project");
   }
 }
