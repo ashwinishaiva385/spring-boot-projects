ca7b3828342d5f4a57e5573388a27872533205b8
Ashwini A <ashwinishaiva385@gmail.com>
2026-08-24 07:53:12 +0530

    Added project using @Controller @RequestMapping @ResponseBody @RestContoller annotation

---
+++ simple1/src/main/java/com/example/simple1/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.simple1;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController 
+{   
+    @RequestMapping("/")
+    @ResponseBody
+    public String greet()
+    {
+    return "Welcome to Ash Projects";
+    }
+    
+}
