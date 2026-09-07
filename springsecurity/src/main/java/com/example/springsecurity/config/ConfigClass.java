baff8947e1b2df8188b604fc8c039207a6cde034
Ashwini A <ashwinishaiva385@gmail.com>
2026-09-07 22:33:05 +0530

    Added spring boot project

---
+++ springsecurity/src/main/java/com/example/springsecurity/config/ConfigClass.java
@@ -0,0 +1,27 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+@Configuration 
+@EnableWebSecurity 
+public class ConfigClass 
+{
+    @Bean 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+   
+       return http
+              .csrf(customizer -> customizer.disable())
+              .authorizeHttpRequests(request ->request.anyRequest().authenticated())
+              //.formLogin(Customizer.withDefaults())
+              .httpBasic(Customizer.withDefaults())
+              .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+              .build();
+}
+}
